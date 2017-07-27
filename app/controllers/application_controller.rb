065b810396dbf68e0a20bb607b54ee6d327140e0
OleksiyNosov <oleksiynosov@gmail.com>
2017-07-27 16:23:20 +0300

    Add basic behavoiur for api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  skip_before_action :verify_authenticity_token
+
+  helper_method :collection, :resource
+
+  def show
+    resource
+  end
+
+  def create
+    build_resource
+
+    resource.save!
+  end
+
+  def update
+    resource.update! resource_params    
+  end
+
+  def destroy
+    resource.destroy!
+  end
 end
