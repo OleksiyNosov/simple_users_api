a061d4275c1e3514eb8488e7e89ebb18c47f3ed3
OleksiyNosov <oleksiynosov@gmail.com>
2017-07-31 21:35:17 +0300

    Add routes for BooksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:index, :show, :create, :update, :destroy]
-
+  resources :users, except: [:create, :edit] do
+    resources :books, except: [:create, :edit] 
+  end
 end
