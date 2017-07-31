b0984351372c69406624088b8396e87048b65f76
OleksiyNosov <oleksiynosov@gmail.com>
2017-07-31 20:52:38 +0300

    Add user reference to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731173829) do
+ActiveRecord::Schema.define(version: 20170731174718) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170731173829) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
