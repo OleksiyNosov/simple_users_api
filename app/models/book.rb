7a9325c18f46e7bc92c762cc7863b31f25a79522
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-01 15:30:07 +0300

    Add Book after_create behavior

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :user_id, presence: true
+
+  after_create :set_default_year
+
+  private
+  def set_default_year
+    self.year ||= Time.now.year
+  end 
 end
