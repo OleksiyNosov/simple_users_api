c1166f77fe6229998bea07136098800ccbc4ab2f
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-04 15:13:50 +0300

    Add decorators for User and Book

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,13 +1,13 @@
 class BookDecorator < ApplicationDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def as_json *args
+    {
+      user_name: user.name,
+      user_id:   user.id,
+      title:     title,
+      author:    author,
+      year:      year 
+    }
+  end
 end
