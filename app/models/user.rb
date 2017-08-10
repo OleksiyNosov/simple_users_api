01e3c16092dfeab998135a87a2c7bc05b3426888
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-10 12:08:41 +0300

    Add validations for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :books
+
+  validates :email, presence: true
+  validates_email_format_of :email
+  validates :email, uniqueness: { case_insensitive: true }
 end
