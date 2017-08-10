f64894c27185c9ea726aa4c456c5a8edb5b5410e
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-10 12:12:37 +0300

    Add tests for models

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should be_an ApplicationRecord }
+
+  it { should belong_to :user }
+
+  it { should validate_presence_of :title }
+
+  it { should validate_presence_of :author }
+
+  describe '#set_default_year' do
+    context do
+      subject { stub_model Book, year: 2000 }
+
+      before { subject.send :set_default_year }
+
+      its(:year) { should eq 2000 }
+    end
+
+    context do 
+      before do
+        #
+        # Time.now.year -> 2007
+        #
+        expect(Time).to receive(:now) do
+          double.tap { |a| expect(a).to receive(:year).and_return(2007) }
+        end        
+      end
+
+      subject { stub_model Book }
+
+      before { subject.send :set_default_year } 
+
+      its(:year) { should eq 2007 }  
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
+require 'validates_email_format_of/rspec_matcher'
 
 RSpec.describe User, type: :model do
+  it { should be_an ApplicationRecord }
+
+  it { should have_many :books }
+
+  it { should validate_presence_of :email }
+  
+  it { should validate_email_format_of :email }
 end
