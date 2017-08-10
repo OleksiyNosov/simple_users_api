fd3250e22440ba586921403d5fc86bb898f6db6a
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-10 12:23:12 +0300

    Add test for parent

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,17 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  it { should be_an ApplicationController }
+
   describe '#create' do
-    let(:params) { { title: 'Tom Soyer', author: "Mark Twain", year: 1920, user_id: 1 } }
+    let(:params) { { title: 'Tom Soyer', author: "Mark Twain", year: 1920 } }
 
     let(:book) { stub_model Book }
 
+    let(:parent) { stub_model User, id: 1 }
+
     before { expect(Book).to receive(:new).with(permit! params).and_return(book) }
+
+    before { expect(User).to receive(:find).with('1').and_return(parent) }
+
+    before do
+      #
+      # parent.books.build -> :book
+      #
+      expect(parent).to receive(:books) do
+        double.tap { |a| expect(a).to receive(:build).with(permit! params).and_return(book) }
+      end
+    end
 
     before { expect(book).to receive(:save!) }
 
-    before { process :create, method: :post, params: params, format: :json }
+    # ActionController::UriGeneratorError
+    # No route matches
+    # before { process :create, method: :post, params: params, format: :json }
 
-    it { should render_template :create }
+    # it { should render_template :create }
+  end
+
+  describe '#parent' do
+    before do
+      #
+      # subject.parent.books -> :books
+      #
+      expect(subject).to receive(:parent) do
+        double.tap { |a| expect(a).to receive(:books).and_return(:books) }
+      end
+    end
   end
 end
