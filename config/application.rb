3216de2f96d857d1a294f32cd6af1d9d40fe17d1
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-10 15:01:26 +0300

    Setup observer

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module SimpleUsersApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.generators.test_framework :rspec
+    config.active_record.observers = :user_observer
+    # config.active_job.adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
