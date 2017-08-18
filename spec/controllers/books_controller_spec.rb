d1de4c4c5d1d7af1484af60f6e9949caebf3cbd9
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-18 12:44:24 +0300

    Add test for index and show

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,6 +3,34 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
   it { should be_an ApplicationController }
 
+  describe '#index' do
+  let(:params) { { user_id: '1' } }    
+
+    before { process :index, method: :get, params: params, format: :json }
+
+    it { should render_template :index }
+  end
+
+  describe '#show' do
+    let(:params) { { id: '1', user_id: '1' } }
+
+    let(:book) { stub_model Book }
+
+    let(:parent) { stub_model User }
+
+    before { expect(User).to receive(:find).with('1').and_return(parent) }
+
+    before do
+      expect(parent).to receive(:books) do
+        double.tap { |a| expect(a).to receive(:find).with('1').and_return(book) }
+      end
+    end
+
+    before { process :show, method: :get, params: params, format: :json }
+
+    it { should render_template :show }
+  end
+
   describe '#create' do
     let(:book_params) { { title: 'Tom Soyer', author: "Mark Twain", year: '1920' } }
 
