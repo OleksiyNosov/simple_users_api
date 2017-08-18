f34e274cc3f748351f03a28c780b7f6c0dbd69ab
OleksiyNosov <oleksiynosov@gmail.com>
2017-08-18 12:19:52 +0300

    Fix test for BooksController#create

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe BooksController, type: :controller do
   it { should be_an ApplicationController }
 
   describe '#create' do
-    let(:params) { { title: 'Tom Soyer', author: "Mark Twain", year: 1920 } }
+    let(:book_params) { { title: 'Tom Soyer', author: "Mark Twain", year: '1920' } }
+
+    let(:params) { { book: book_params, user_id: '1' } }
 
     let(:book) { stub_model Book }
 
-    let(:parent) { stub_model User, id: 1 }
-
-    before { expect(Book).to receive(:new).with(permit! params).and_return(book) }
+    let(:parent) { stub_model User }
 
     before { expect(User).to receive(:find).with('1').and_return(parent) }
 
@@ -19,17 +19,16 @@ RSpec.describe BooksController, type: :controller do
       # parent.books.build -> :book
       #
       expect(parent).to receive(:books) do
-        double.tap { |a| expect(a).to receive(:build).with(permit! params).and_return(book) }
+        double.tap { |a| expect(a).to receive(:build).with(permit! book_params).and_return(book) }
       end
     end
 
     before { expect(book).to receive(:save!) }
 
-    # ActionController::UriGeneratorError
-    # No route matches
-    # before { process :create, method: :post, params: params, format: :json }
+   
+    before { process :create, method: :post, params: params, format: :json }
 
-    # it { should render_template :create }
+    it { should render_template :create }
   end
 
   describe '#parent' do
